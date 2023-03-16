2181bada356c6cd0df55d2c2a596eca46dd617d0
chloew3st <chloew3st@gmail.com>
2023-03-16 12:13:15 -0500

    exercise one commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var startMyDay = function() {
+    return "Time for coffee and stretching!";
+};
+console.log(startMyDay());
+
+var favoriteCookie = function(cookie) {
+    return `My favorite cookie is ${cookie}`;
+};
+console.log(favoriteCookie());
+console.log(favoriteCookie("peanutbutter"));
